78511f36553ce0e92d1598de1ee7fc365164d82c
raynaldo-deanda <reynaldo.deanda@gmail.com>
2022-12-08 09:16:07 -0600

    update w/ loading function

--- moviejs.js
+++ moviejs.js
@@ -174,3 +174,57 @@ function deleteMovie(search) {
         })
 
 }
+
+
+
+
+
+//-------------------------------FETCH REQUEST LOADING ANIMATION-----------------------------------
+// // selecting dom element
+//     const textInput = document.querySelector("#inputPart");
+// const textOutput = document.querySelector("#showOutput");
+// const btn = document.querySelector("#submitInput");
+//
+// // adding event listener to button
+// btn.addEventListener("click", fetchHandler);
+//
+// // selecting loading div
+// const loader = document.querySelector("#loading");
+//
+// // showing loading
+// function displayLoading() {
+//     loader.classList.add("display");
+//     // to stop loading after some time
+//     setTimeout(() => {
+//         loader.classList.remove("display");
+//     }, 5000);
+// }
+//
+// // hiding loading
+// function hideLoading() {
+//     loader.classList.remove("display");
+// }
+//
+// // dummy url
+// const url = "https://www.neighborly-delirious-fin.glitch.me/movies"
+//
+// function fetchHandler(event) {
+//     displayLoading()
+//     let input = textInput.value;
+//     let finalURL = buildURL(input);
+//
+//     fetch(finalURL)
+//         .then(response => response.json())
+//         .then(json => {
+//             hideLoading()
+//             textOutput.innerText = json.contents.translated;
+//         })
+// }
+// // creating url format
+// // we need
+// // https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="your input"
+//
+// function buildURL(inputData) {
+//     return `${url}?text=${inputData}`;
+// }
+//
